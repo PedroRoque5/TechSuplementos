00ebcddfe4afc67f2df04365c1507102076493e7
PedroRoque5 <pedroeduardoroque01@gmail.com>
2024-08-22 14:24:24 -0300

    Atualização do corpo do projeto

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+//Requerimento para funcionamento
+require '.vedor/autoload.php';
+require_once './config_serve.php';
+//Variavel para receber uma página, caso não receba, apresenta a tela home
+$pagina = isset($_GET['pg']) ? $_GET['pg'] : 'home';
+
+include_once '.View/tamplate/cabecalho.php';
+
+/***
+ * 
+ * Switch Case para as páginas
+ * 
+ * Utilizar parametro ($pagina)
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+include_once './View/template/rodape.php';
