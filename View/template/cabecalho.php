73cd7adf8519d509d7684c667332f8335b555f25
PedroRoque5 <pedroeduardoroque01@gmail.com>
2024-08-23 14:28:31 -0300

    Atualização

--- View/template/cabecalho.php
+++ View/template/cabecalho.php
@@ -3,12 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tech Suplementos</title>
+    <title><img src=""> Tech Suplementos</title>
 </head>
 <body>
 
 <?php
-
-echo "Cabecalho do site";
 
 ?>
