a3bca36f5857c913900bd5e9226bf106908fef03
GigiMoyses005 <gigi.moyses@gmail.com>
2024-08-28 15:42:30 -0300

    cabeçalho

--- View/template/cabecalho.php
+++ View/template/cabecalho.php
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><img src=""> Tech Suplementos</title>
+    <link rel="stylesheet" href="../../public/./assets/./css/style.css">
+    <title><img src="../../public/./assets/./image/halteres (1).png"> Tech Suplementos</title>
 </head>
+
 <body>
+    <div id=menu>
+        <img src="../../public/./assets/././image/img.jpg">
+        <nav id="menu">
+   
+      <input id="search" type="search" placeholder="O que você procura?">
+      <button type="submit">pesquisar</button>
 
-<?php
+</nav>
+            
+        <nav id="menu">
+            <ul>
+                <li><a href="Home">Home</a></li>
+                <li><a href="Login">Login</a></li>
+                <li><a href="Cadastro">Cadastro</a></li>
+                <li><a href="Suporte">Suporte</a></li>
+                <li><a href="carinho">Carrinho</a></li>
+            </ul>
+        </nav>
 
-?>
+    </div>
+    <?php
+
+    ?>
