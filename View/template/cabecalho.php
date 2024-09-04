a8c0190a9084c88162aa0afcd204c341f168ce60
GigiMoyses005 <gigi.moyses@gmail.com>
2024-09-04 14:07:00 -0300

    cabeçalho

--- View/template/cabecalho.php
+++ View/template/cabecalho.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../public/./assets/./css/style.css">
+    <link rel="stylesheet" href="../../public/./assets/./css/cabecalho.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     <title><img src="../../public/./assets/./image/halteres (1).png"> Tech Suplementos</title>
