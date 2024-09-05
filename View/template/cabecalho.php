b77185aa8c614c3334c1e3a1b5bde30e21b03219
GigiMoyses005 <gigi.moyses@gmail.com>
2024-09-05 14:56:39 -0300

    Cabeçalho atualizado

--- View/template/cabecalho.php
+++ View/template/cabecalho.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -24,13 +23,34 @@
         </nav>
         <nav id="menu">
             <ul id="principal">
-                <li id="unico"><a href="../home/index.php"><i class="fa-solid fa-house"></i>Home</a></li>
-                <li id="unico"><a href="Login" href="Cadastro"><i class="fa-solid fa-user"></i>Login/Cadastro</a></li>
-                <li id="unico"><a href="Suporte"><i class="fa-solid fa-question"></i>Suporte</a></li>
+                <li id="unico"><a href="Home"><i class="fa-solid fa-house"></i>Home</a></li>
+                <li id="unico"><a href="Login" href="Cadastro"><i class="fa-solid fa-user"></i>Login/Cadastro</a> </li>
+                <li id="unico"><a href="Suporte"><i class="fa-solid fa-question"></i>Suporte</a>
+                </li>
                 <li id="unico"><a href="carinho"><i class="fa-solid fa-cart-shopping"></i></a></li>
             </ul>
         </nav>
     </div>
-    <?php 
-    
+    <div id="sub">
+        <nav id="submenu">
+            <ul id="segunda">
+                <li id="segundo">
+                    <a href="#">Todas as categorias</a>
+                    <ul class="dropdown-menu" id=navcascata>
+                        <li  class="cascata"><a class="dropdown-item" href="#" >Action</a></li>
+                        <li  class="cascata"><a class="dropdown-item" href="#">Another action</a></li>
+                        <li  class="cascata"><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li id="segundo"><a href="top">Top 5</a></li>
+                <li id="segundo"><a href="novidades" href="Cadastro">Novidades</a></li>
+                <li id="segundo"><a href="proteina">Proteína</a></li>
+                <li id="segundo"><a href="creatina">Creatina</a></li>
+                <li id="segundo"><a href="pre">Pré-treino</a></li>
+                <li id="segundo"><a href="metas">Metas</a></li>
+            </ul>
+        </nav>
+    </div>
+    <?php
+
     ?>
