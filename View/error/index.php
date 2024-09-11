d29abfb716773620424dffa9e4429c54e9fceb51
PedroRoque5 <pedroeduardoroque01@gmail.com>
2024-09-11 14:37:51 -0300

    Criação da página de erro e seu respectivo arquivo css

---
+++ View/error/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404</title>
+    <link rel="stylesheet" href="../../public/assets/css/error.css">
+</head>
+<body>
+
+    <h1 id="titulo_error">Página não encontrada</h1>
+
+    <h1 id="codigo_error">404</h1>
+
+    <button type="button" id="return_home">Voltar a página principal</button>
+    
+</body>
+</html>
