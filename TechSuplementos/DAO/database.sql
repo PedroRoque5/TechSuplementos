c89841c5b2f73b4b2d851fed6fa554ec9b414918
PedroRoque5 <pedroeduardoroque01@gmail.com>
2024-10-23 10:58:01 -0300

    Criação do database.sql

---
+++ TechSuplementos/DAO/database.sql
@@ -0,0 +1,73 @@
+-- Copiando estrutura do banco de dados para techsuplementos
+CREATE DATABASE IF NOT EXISTS `techsuplementos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `techsuplementos`;
+
+-- Copiando estrutura para tabela techsuplementos.compra
+CREATE TABLE IF NOT EXISTS `compra` (
+  `Forma de pagamento` varchar(50) DEFAULT NULL,
+  `Id` int(10) DEFAULT NULL,
+  `Quantidade de produtos` int(255) DEFAULT NULL,
+  `idEstoque` int(10) DEFAULT NULL,
+  `idUsuario` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela techsuplementos.estoque
+CREATE TABLE IF NOT EXISTS `estoque` (
+  `Id` int(10) DEFAULT NULL,
+  `Nome` varchar(255) DEFAULT NULL,
+  `Quantidade` int(255) DEFAULT NULL,
+  `idProduto/marca` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela techsuplementos.feedback
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `id` int(10) DEFAULT NULL,
+  `Avaliações` text DEFAULT NULL,
+  `idUsuario` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela techsuplementos.gestão de pedido
+CREATE TABLE IF NOT EXISTS `gestão de pedido` (
+  `id` int(10) DEFAULT NULL,
+  `Histórico de Compra` varchar(255) DEFAULT NULL,
+  `Histórico do pagamento` varchar(255) DEFAULT NULL,
+  `idCompra` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela techsuplementos.produto/marca
+CREATE TABLE IF NOT EXISTS `produto/marca` (
+  `ID` int(10) DEFAULT NULL,
+  `Ano de fundação` date DEFAULT NULL,
+  `Nome` varchar(50) DEFAULT NULL,
+  `País de origem` varchar(50) DEFAULT NULL,
+  `idPromoções/descontos` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela techsuplementos.promoção/desconto
+CREATE TABLE IF NOT EXISTS `promoção/desconto` (
+  `id` int(10) DEFAULT NULL,
+  `Valor` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela techsuplementos.usuário
+CREATE TABLE IF NOT EXISTS `usuário` (
+  `Telefone` int(11) DEFAULT NULL,
+  `E-Mail` varchar(50) DEFAULT NULL,
+  `CPF/CNPJ` int(11) DEFAULT NULL,
+  `Nome` varchar(50) DEFAULT NULL,
+  `Senha` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
