29befe7416b1fc18326b21385c80be7044fbd165
PedroRoque5 <pedroeduardoroque01@gmail.com>
2024-10-30 07:16:55 -0300

    Ajuste do CPF e Número de telefone da página cadastro

--- View/cadastro/index.php
+++ View/cadastro/index.php
@@ -1,13 +1,12 @@
  <link href="<?= ASSETS ?>css/pessoa.css" rel='stylesheet'>
-<div class="form">
-    <form>
-    <div id="email"><input type="email" placeholder="E-mail*"></div>
-    <div><input type="text" placeholder="Nome*"></div>
-    <div><input type="number" placeholder="CPF*"></div>
-    <div><input type="number" placeholder="Telefone*"></div>
-    <div><input type="password" placeholder="Senha"></div>
-      <a class="btn btn-success" href="<?= URL . 'index.php?pg=login' ?>">Login</a>
-      <a  href="<?= URL . "index.php?pg=home"?>"class="btn btn-input">Cancelar</a>
-    </form>
-</div>
-
+ <div class="form">
+   <form>
+     <div id="email"><input type="email" placeholder="E-mail*"></div>
+     <div><input type="text" placeholder="Nome*"></div>
+     <div><input type="number" placeholder="CPF*" maxlength="11" oninput="if(this.value.length > 11) this.value = this.value.slice(0, 11)"></div>
+     <div><input type="tel" placeholder="Telefone*" maxlength="14" pattern="\d{10,11}" title="Insira um telefone válido"></div>
+     <div><input type="password" placeholder="Senha"></div>
+     <a class="btn btn-success" href="<?= URL . 'index.php?pg=login' ?>">Login</a>
+     <a href="<?= URL . "index.php?pg=home" ?>" class="btn btn-input">Cancelar</a>
+   </form>
+ </div>
